a8d870268bbbdfbe7b4ba2556d02d5075f76e795
elvan  celik <lvn@elvans-MacBook-Air.local>
2021-06-06 15:24:33 -0500

    june6second

---
+++ src/main/java/com/techproedu4/springmvc4/ResultController.java
@@ -0,0 +1,24 @@
+package com.techproedu4.springmvc4;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ResultController {
+	
+	@RequestMapping("add")
+	public String sumOfNumber(HttpServletRequest reg) {
+		
+		int a = Integer.parseInt(reg.getParameter("n1"));
+		int b = Integer.parseInt(reg.getParameter("n2"));
+		int sum = a+b;
+		HttpSession session = reg.getSession();
+		session.setAttribute("sumOfNum", sum);
+		
+		return "result.jsp";
+	}
+
+}
