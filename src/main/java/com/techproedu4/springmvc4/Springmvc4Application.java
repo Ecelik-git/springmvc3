d708184533d8fdb3766b9b98d583b97c532d442c
elvan  celik <lvn@elvans-MacBook-Air.local>
2021-06-06 13:19:58 -0500

    june6

---
+++ src/main/java/com/techproedu4/springmvc4/Springmvc4Application.java
@@ -0,0 +1,13 @@
+package com.techproedu4.springmvc4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmvc4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springmvc4Application.class, args);
+	}
+
+}
